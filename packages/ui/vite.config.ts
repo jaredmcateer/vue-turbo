c2cf0555a14742b0fe5f4301500fdc193f44db85
Jared McAteer <jared.mcateer@gmail.com>
2022-01-16 15:50:45 -0700

    Enable vite library mode on ui components

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "UI",
+      fileName: (format) => `ui.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
